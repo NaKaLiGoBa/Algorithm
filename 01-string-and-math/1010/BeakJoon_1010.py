171a117fc4f9916747ee2ee123f12b306d449855
zerochoi <jchoi4570@naver.com>
2023-07-13 12:50:24 +0900

    [진영] 다리놓기

---
+++ 01-string-and-math/1010/BeakJoon_1010.py
@@ -0,0 +1,14 @@
+# def factorial(n):
+#     num = 1
+#     for i in range(1, n+1):
+#         num *= i
+#     return num
+
+import math
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
+    print(bridge)
