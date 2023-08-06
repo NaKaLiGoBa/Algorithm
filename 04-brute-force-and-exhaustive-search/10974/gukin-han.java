5dab4f6c5cb321b342bc7a52a3ccaf977fb6d2bf
Gukin Han <gukin.dev@gmail.com>
2023-08-06 20:14:36 +0900

    [국인] 모든 순열

---
+++ 04-brute-force-and-exhaustive-search/10974/gukin-han.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static int[] array;
+    private static int N;
+    private static boolean[] visited;
+    private static List<Integer> temp = new ArrayList<>();
+    private static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        visited = new boolean[N];
+        array = IntStream.rangeClosed(1, N).toArray();
+        permutation(0);
+        System.out.println(answer.toString());
+    }
+
+    private static void permutation(int numOfElement) {
+        if (numOfElement == N) {
+            for (int i = 0; i < temp.size(); i++) {
+                answer.append(temp.get(i)).append(" ");
+            }
+            answer.append("\n");
+        }
+
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                temp.add(i + 1);
+                permutation(numOfElement + 1);
+                visited[i] = false;
+                temp.remove(temp.size() - 1);
+            }
+        }
+
+    }
+}
