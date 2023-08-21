5982d0611b95df2574b013d8221bb1ded340ea7f
Gyujin Kim <kjkj268@naver.com>
2023-08-21 12:34:01 +0900

    문자열 나누기

---
+++ 04-brute-force-and-exhaustive-search/195688/gyujin.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	private static int N;
+	private static String str;
+	private static ArrayList<String> list;
+	private static HashSet<String> set;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		str = br.readLine();
+		
+		set = new HashSet<>();      // 중복 제거하기 위해 HashSet 사용. 
+		
+		scoreBoard();               // P를 만들기 위함.
+        
+		list = new ArrayList<>(set);        // 중복을 제거한 HashSet을 정렬하기 위해 ArrayList에 담음.
+		Collections.sort(list);             // 오름차순 정렬.
+	
+		System.out.println(scoreMax());     // scoreBoard에서 했던거와 같이 scoreMax도 브루트포스로 검사하여 최대값을 return
+	}
+	
+	private static void scoreBoard() {          
+			for (int i = 1; i < N - 1; i++) {
+				for(int j = i + 1; j < N; j++) {
+					String first = str.substring(0, i);
+					String second = str.substring(i, j);
+					String third = str.substring(j);
+				
+					set.add(first);
+					set.add(second);
+					set.add(third);
+			}
+		}
+	}
+	
+	private static int scoreMax() {
+		int maxScore = Integer.MIN_VALUE;
+		
+		for (int i = 1; i < N - 1; i++) {
+			for(int j = i + 1; j < N; j++) {
+				String first = str.substring(0, i);
+				String second = str.substring(i, j);
+				String third = str.substring(j);
+				
+				int score = list.indexOf(first) + 1
+					+ list.indexOf(second) + 1
+					+ list.indexOf(third) + 1;
+				
+				maxScore = Math.max(maxScore, score);
+			}
+		}
+		return maxScore;
+	}
+	
+}
